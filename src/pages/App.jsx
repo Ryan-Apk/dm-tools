f675c253823addc4452eb38560a4bb346f8b07e4
MrIvory124 <blueturtle124@gmail.com>
2026-06-14 19:28:23 +1200

    After some decision, not embedding excalidraw is the easiest option, so this is a push pre design phase begin

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,5 +1,4 @@
-import {NavLink, Route, Routes} from 'react-router-dom'
-import Whiteboard from "./Whiteboard.jsx";
+import {Route, Routes} from 'react-router-dom'
 import NotFound from "./NotFound.jsx";
 
 /// This is the file that returns the full function of the program, should only contain routes
@@ -9,13 +8,9 @@ export default function App() {
                 {/*The below routes to the home.jsx page */}
                 <Route path="/" component={App}
                        element={<div className="flex flex-col items-center justify-center h-full p-10">
-                           <NavLink to="/whiteboard/1"
-                                    className="rounded-xl border-2 p-5 border-black text-xl font-bold bg-red-700 text-white">Take
-                               me to the battlemap!</NavLink>
-                           <h1 className="mt-4 font-bold">For now this link goes to the global whiteboard, in the future it will go elsewhere!</h1>
+                           <h1 className="mt-4 font-bold">Test</h1>
                        </div>}/>
-                <Route path="/whiteboard" element={<Whiteboard/>}/>
-                <Route path="/whiteboard/:whiteboardId" element={<Whiteboard/>}/>
+                <Route path="/button" element={<NotFound/>}/>
                 <Route path="*" element={<NotFound/>}/>
             </Routes>
 
--- src/pages/Whiteboard.jsx
+++
@@ -1,7 +0,0 @@
-export default function Whiteboard() {
-    return (
-        <div className="fixed inset-0 bg-neutral-200">
-            test
-        </div>
-    )
-}
