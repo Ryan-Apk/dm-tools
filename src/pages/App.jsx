b1eda79221806f413228c3e6e2249d05b89ea3c1
Ryan <blueturtle124@gmail.com>
2026-06-22 10:12:55 +1200

    app.jsx changed so that path leads to new test button - added some css and button component - started work on the navbar

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,13 @@
 import {Route, Routes} from 'react-router-dom'
 import NotFound from "./NotFound.jsx";
 import BugList from "./BugList.jsx";
+import Button from "../components/Button.jsx";
+import NavBar from "../components/NavBar.jsx";
 
 /// This is the file that returns the full function of the program, should only contain routes
 export default function App() {
     return (<div>
+        <NavBar/>
             <Routes>
                 {/*The below routes to the home.jsx page */}
                 <Route path="/" component={App}
@@ -12,6 +15,7 @@ export default function App() {
                            <h1 className="mt-4 font-bold">Test</h1>
                        </div>}/>
                 <Route path="/bugs" element={<BugList/>}/>
+                <Route path="/button" element={<Button>Hi</Button>}/>
                 <Route path="*" element={<NotFound/>}/>
             </Routes>
         </div>)
